bef9325f17d0da5c94b32f6384e1fc4f9253f77b
franklynekirwa2 <sangkirwa2@gmail.com>
2024-07-02 23:14:36 +0300

    arraylab

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Write your solution here!
+let cats=["Milo", "Otis", "Garfield"];
+
+function Arrays(){
+    cats.length=0;
+    cats.push("Milo", "Otis", "Garfield");
+}
+
+ function destructivelyAppendCat(){
+
+    cats.push("Ralph");
+    return cats;
+ }
+ console.log(cats);
+
+function destructivelyPrependCat(){
+        cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    return [...cats, name];
+}
+
+function prependCat(name){
+    return [name, ...cats];
+}
+function removeLastCat(){
+    return cats.slice(0, cats.length -1);
+}
+
+function removeFirstCat(){
+    return cats.slice(1);
+}
+console.log(cats);
+
